8b04bde9a5869920b9d0831ad0920c56a34be110
yangyanyin <yangyanyin@aliyun.com>
2021-05-04 20:58:08 +0800

    n

--- src/components/base/MessagePlate.vue
+++ src/components/base/MessagePlate.vue
@@ -2,7 +2,7 @@
   <div class="message">
     <div class="content">
       <p class="t">新加坡全球护照交流中心： </p>
-      <p>直营新加坡移民/银行开户/新加坡留学，家族办公室，欧盟&英联邦快速护照，不成功不收。</p>
+      <p>直营新加坡移民/银行开户/新加坡留学，家族办公室，欧盟&英联邦快速护照，不成功，不收费</p>
       <a target="_blank" href="https://waterlandcap.com/">点击了解更多详情</a>
     </div>
   </div>
